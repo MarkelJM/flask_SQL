b2e2be3d7e4295806ab0af8a589301f82f31bbd6
MarkelJM <markjuaristi@gmail.com>
2022-07-14 20:19:21 +0200

    feat: comienzo clase 14/07

--- balance/forms.py
+++ balance/forms.py
@@ -1,12 +1,20 @@
-from flask_wtf  import FlaskForm
-from wtforms import  DateField, DecimalField, HiddenField, RadioField, StringField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import DateField, FloatField, HiddenField, RadioField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class MovimientosForm(FlaskForm):
     id = HiddenField()
-    fecha = DateField("Fecha")
-    concepto = StringField("Concepto")
-    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")])
-    cantidad = DecimalField("Cantidad", places=2)
+    fecha = DateField("Fecha", validators=[DataRequired(message="Debes introducir una fecha")])
+    concepto = StringField(
+        "Concepto", validators=[
+            DataRequired("Debes escribir un concepto"),
+            Length(min=3, max=25, message="Debe tener entre 3 y 25 caracteres")
+        ])
+    tipo = RadioField(
+        choices=[("I", "Ingreso"), ("G", "Gasto")],
+        validators=[DataRequired(message="¿Es un gasto o un ingreso?")]
+    )
+    cantidad = FloatField("Cantidad", validators=[DataRequired(message="Debes especificar un número")])
 
-    submit = SubmitField("Guardar")
+    submit = SubmitField("Guardar", render_kw={ "class": "blue-button" })
--- balance/views.py
+++ balance/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, request
+from datetime import date
 
+from flask import flash, render_template, redirect, request, url_for
 
-from .forms import MovimientosForm
 from . import app
+from .forms import MovimientosForm
 from .models import DBManager
 
 
@@ -21,20 +22,44 @@ def inicio():
 
 @app.route("/nuevo", methods=["GET", "POST"])
 def nuevo():
-    return "Crear movimiento"
+    return render_template("nuevo.html")
 
 
 @app.route("/modificar/<int:id>", methods=["GET", "POST"])
 def actualizar(id):
     if request.method == "GET":
-        formulario = MovimientosForm()
-        return render_template("form_movimiento.html", form=formulario)
+        db = DBManager(RUTA)
+        movimiento = db.obtenerMovimientoPorId(id)
 
-    return f"Actualizar el movimiento con ID={id}"
+        movimiento["fecha"] = date.fromisoformat(movimiento["fecha"])
+
+        formulario = MovimientosForm(data=movimiento)
+        return render_template("form_movimiento.html", form=formulario, id=id)
+
+    elif request.method == "POST":
+        form = MovimientosForm(data=request.form)
+        if form.validate():
+            db = DBManager(RUTA)
+            consulta = "UPDATE movimientos SET cantidad=?, concepto=?, tipo=?, fecha=? WHERE id=?"
+            params = (
+                form.cantidad.data,
+                form.concepto.data,
+                form.tipo.data,
+                form.fecha.data,
+                form.id.data)
+            resultado = db.consultaConParametros(consulta, params)
+            if resultado:
+                flash("Movimiento actualizado correctamente ;)", category="exito")
+                return redirect(url_for("inicio"))
+            return render_template("form_movimiento.html", form=form, id=id, errores=["Ha fallado la operación de guardar en la base de datos"])
+        else:
+            return render_template("form_movimiento.html", form=form, id=id, errores=["Ha fallado la validación de los datos"])
 
 
 @app.route("/borrar/<int:id>", methods=["GET", "POST"])
 def eliminar(id):
     db = DBManager(RUTA)
-    esta_borrado = db.borrar(id)
-    return render_template("borrar.html", resultado=True)
+    consulta = "DELETE FROM movimientos WHERE id=?"
+    params = (id,)
+    esta_borrado = db.consultaConParametros(consulta, params)
+    return render_template("borrar.html", resultado=esta_borrado)
